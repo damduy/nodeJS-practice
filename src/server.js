eeece75044af1370f2a9ca0cb207a15c6bb4f7ad
damduy <80811184+damduy@users.noreply.github.com>
2025-07-21 13:58:57 +0700

    Add View Engine

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const path = require('path')
+
+
 const app = express()
 const port = 6969
+
+//config template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 //khai bao route
 app.get('/', (req, res) => {
@@ -12,7 +19,8 @@ app.get('/xyz', (req, res) => {
 })
 
 app.get('/test', (req, res) => {
-  res.send('<h1> Add h1 tag </h1>')
+  // res.send('<h1> Add h1 tag </h1>')
+  res.render('sample.ejs')
 })
 
 app.listen(port, () => {

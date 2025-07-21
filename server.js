13a1fd7260cd16870904f726bbf5fb0cfd5d4aaa
damduy <80811184+damduy@users.noreply.github.com>
2025-07-21 10:08:06 +0700

    Add more route

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 6969
 
+//khai bao route
 app.get('/', (req, res) => {
   res.send('Hello World! by Duy')
+})
+
+app.get('/xyz', (req, res) => {
+  res.send('Check xyz')
+})
+
+app.get('/test', (req, res) => {
+  res.send('<h1> Add h1 tag </h1>')
 })
 
 app.listen(port, () => {

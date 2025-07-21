5d6b993255709f708a2f0b6366eddafe56ea290c
damduy <80811184+damduy@users.noreply.github.com>
2025-07-21 17:55:14 +0700

    config static file

--- src/server.js
+++ src/server.js
@@ -11,6 +11,10 @@ const hostname = process.env.HOST_NAME
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+//config static file
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 //khai bao route
 app.get('/', (req, res) => {
   res.send('Hello World! by Duy & nodemon')

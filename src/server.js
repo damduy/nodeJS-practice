342eddb4431c8920e01a2a26ddb53218b8db43cb
damduy <80811184+damduy@users.noreply.github.com>
2025-07-21 14:32:04 +0700

    Add env

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config()
 
 
 const app = express()
-const port = 6969
+const port = process.env.PORT || 8888
+const hostname = process.env.HOST_NAME
 
 //config template engine
 app.set('views', path.join(__dirname, 'views'))
@@ -23,6 +25,6 @@ app.get('/test', (req, res) => {
   res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
